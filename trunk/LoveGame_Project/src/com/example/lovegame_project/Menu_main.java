b7534cf54c8cda16a0a5ed16c32a4a3aa3f488cc
julienbreno.nave@gmail.com <julienbreno.nave@gmail.com@30d8be19-2334-6981-95c7-94f7bf786c54>
2013-04-17 23:14:26 +0000

    Init

---
+++ trunk/LoveGame_Project/src/com/example/lovegame_project/Menu_main.java
@@ -0,0 +1,54 @@
+package com.example.lovegame_project;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+import android.content.*;
+
+public class Menu_main extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_menumain);
+		Button	btcomecar = (Button)findViewById (R.id.bt_comecar);
+		Button btinstrucoes = (Button)findViewById (R.id.bt_comecar);
+		Button btcreditos = (Button)findViewById (R.id.bt_comecar);
+		
+		//Button Comecar
+		btcomecar.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				try{
+				Intent trocatela = new Intent(Menu_main.this,Love_Game.class);
+				Menu_main.this.startActivity(trocatela);
+				Menu_main.this.finish();
+				}catch(Exception e)
+				{
+					Mensagem("erro"+e.getMessage());
+				}
+				
+			}
+			
+		});
+		
+		
+	}
+	private void Mensagem(String msg) {
+		//  apenas informa no rodapé inferior da tela do Android o ocorrido
+		Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+	}
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.menu_main, menu);
+		return true;
+	}
+
+}
