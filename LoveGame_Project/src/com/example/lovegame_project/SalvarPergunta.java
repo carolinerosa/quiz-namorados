8ac2dab1c1d368d8ffb07956d3f40246bd3d264d
julienbreno.nave@gmail.com <julienbreno.nave@gmail.com@30d8be19-2334-6981-95c7-94f7bf786c54>
2013-04-18 01:42:39 +0000

    Revisões de debug e context

--- LoveGame_Project/src/com/example/lovegame_project/SalvarPergunta.java
+++ LoveGame_Project/src/com/example/lovegame_project/SalvarPergunta.java
@@ -11,32 +11,46 @@ import android.widget.TextView;
 public class SalvarPergunta extends Activity{
 	private TextView txtSalvar= (TextView) findViewById(R.id.edtSalvar);
 	private TextView txtNomeArq=(TextView) findViewById(R.id.edtNomeArq);
+	public String lstrNomeArq;
+    public File arq;
+    public byte[] dados;
+	//Singleton Area
+	private static SalvarPergunta Instance;
+	private SalvarPergunta()
+	{
+		
+	}
+	static SalvarPergunta GetInstance()
+	{
+		return Instance;
+	}
 	
-	public void click_Salvar()
+	public void Salvar()
 	{
 		//pega o texto criado, e o diretorio(file),pega os bytes do texto e manda usando o FileOutputStream, dando o diretório e o byte a ser enviado.
-		String lstrNomeArq;
-	     File arq;
-	     byte[] dados;
-	      try
-	      {
-	          lstrNomeArq = txtNomeArq.getText().toString();
-	             
-	arq = new File(Environment.getExternalStorageDirectory(), lstrNomeArq);
-	          FileOutputStream fos;
-	             
-	dados = txtSalvar.getText().toString().getBytes();
-	             
-	          fos = new FileOutputStream(arq);
-	          fos.write(dados);
-	          fos.flush();
-	          fos.close();
-	          DebugLog.Get_Instance().Mensagem("Texto Salvo com sucesso!");
-	         // Love_Game.GetInstance().Listar();
-	      } 
-	      catch (Exception e) 
-	      {
-	    	  DebugLog.Get_Instance().Mensagem("Erro : " + e.getMessage());
-	      }     
-	}
+				String lstrNomeArq;
+			     File arq;
+			     byte[] dados;
+			      try
+			      {
+			          lstrNomeArq = txtNomeArq.getText().toString();
+			             
+			arq = new File(Environment.getExternalStorageDirectory(), lstrNomeArq);
+			          FileOutputStream fos;
+			             
+			dados = txtSalvar.getText().toString().getBytes();
+			             
+			          fos = new FileOutputStream(arq);
+			          fos.write(dados);
+			          fos.flush();
+			          fos.close();
+			          DebugLog.Get_Instance().Mensagem("Texto Salvo com sucesso!");
+			          //Listar();
+			      } 
+			      catch (Exception e) 
+			      {
+			    	  DebugLog.Get_Instance().Mensagem("Erro : " + e.getMessage());
+			      }   
+}
+	
 }
