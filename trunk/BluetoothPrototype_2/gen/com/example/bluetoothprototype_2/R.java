403dbbabedecea104955d23e6b9e8b3a4200e880
thauanlopes.nave <thauanlopes.nave@30d8be19-2334-6981-95c7-94f7bf786c54>
2013-04-21 16:39:03 +0000

    Release de protótipo:
    ligando/desligando o bluetooth
    descobrindo novos dispositivos
    pegando os dispositivos parentiados
    comecando a fazer a conexao

--- trunk/BluetoothPrototype_2/gen/com/example/bluetoothprototype_2/R.java
+++ trunk/BluetoothPrototype_2/gen/com/example/bluetoothprototype_2/R.java
@@ -24,23 +24,40 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
-        public static final int activateBluetooth=0x7f080000;
-        public static final int btDevicesSpinner=0x7f080001;
-        public static final int desactivateBluetooth=0x7f080002;
+        public static final int ScanButton=0x7f080006;
+        public static final int action_settings=0x7f080008;
+        public static final int activateBtButton=0x7f080005;
+        public static final int button_scan=0x7f080004;
+        public static final int leaveButton=0x7f080007;
+        public static final int newDevicesTittle=0x7f080002;
+        public static final int new_devices=0x7f080003;
+        public static final int pairedDevicesTittle=0x7f080000;
+        public static final int paired_devices=0x7f080001;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_device_list=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int list=0x7f030002;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int device_list=0x7f070000;
+        public static final int main=0x7f070001;
     }
     public static final class string {
+        public static final int NewDevices=0x7f050005;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
-        public static final int turnBtOff=0x7f050003;
-        public static final int turnBtOn=0x7f050004;
+        public static final int connect=0x7f050008;
+        public static final int leave=0x7f050009;
+        public static final int listaDePareados=0x7f050004;
+        public static final int scanButton=0x7f050003;
+        /**      Device List 
+         */
+        public static final int title_activity_device_list=0x7f050002;
+        public static final int turn_bt_off=0x7f050007;
+        /**      Activity Principal 
+         */
+        public static final int turn_bt_on=0x7f050006;
     }
     public static final class style {
         /** 
